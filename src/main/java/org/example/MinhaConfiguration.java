75f77ab7319a632509a6e517ca591e6b746979f1
rodlferreira <rodriggo.lferreira@icloud.com>
2022-10-08 17:16:36 -0300

    "Finalização do tópico 4 Configurations e Beans no Spring Boot"

---
+++ src/main/java/org/example/MinhaConfiguration.java
@@ -0,0 +1,12 @@
+package org.example;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinhaConfiguration {
+
+    @Bean (name = "applicationName")
+    public String applicationName(){
+        return "Sistema de Vendas";
+    }
+}
