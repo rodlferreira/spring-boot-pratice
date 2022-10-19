9e526081e7ae19fc6052eb94eef2e370653d46b5
rodlferreira2 <rodriggo.lferreira@icloud.com>
2022-10-19 19:38:14 -0300

    Finalização da aula 10 injeções de dependências

---
+++ src/main/java/org/example/repository/ClientesRepository.java
@@ -0,0 +1,11 @@
+package org.example.repository;
+
+import org.example.model.Cliente;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClientesRepository {
+    public void persistir(Cliente cliente) {
+        //acessa a base e salva o cliente
+    }
+}
