960978e38f85e2836a7c01df15fe3019bb4379ed
rodlferreira2 <rodriggo.lferreira@icloud.com>
2022-10-27 11:01:03 -0300

    configurando o application.properties

--- src/main/java/org/example/MinhaConfiguration.java
+++ src/main/java/org/example/MinhaConfiguration.java
@@ -4,9 +4,4 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MinhaConfiguration {
-
-    @Bean (name = "applicationName")
-    public String applicationName(){
-        return "Sistema de Vendas";
-    }
 }
