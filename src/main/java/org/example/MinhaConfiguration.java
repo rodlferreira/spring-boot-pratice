33c06c9387e4c369e291b2804d614ab9f7cea8d5
rodlferreira2 <rodriggo.lferreira@icloud.com>
2022-11-16 22:06:38 -0300

    subindo ultimas atualizações para formatação do PC

--- src/main/java/org/example/MinhaConfiguration.java
+++ src/main/java/org/example/MinhaConfiguration.java
@@ -1,7 +1,17 @@
 package org.example;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development")
 public class MinhaConfiguration {
+
+    @Bean
+    public CommandLineRunner executar(){
+        return args -> {
+            System.out.println("Rodando a configuração de desenvolvimento");
+        };
+    }
 }
