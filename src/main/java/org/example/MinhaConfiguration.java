91148c9022e43231f4670c6415eff914f8279d68
rodlferreira <rodriggo.lferreira@icloud.com>
2022-11-19 12:05:51 -0300

    Aula 14 iniciada

--- src/main/java/org/example/MinhaConfiguration.java
+++ src/main/java/org/example/MinhaConfiguration.java
@@ -1,11 +1,9 @@
 package org.example;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
-@Configuration
-@Profile("development")
+
+@Development
 public class MinhaConfiguration {
 
     @Bean
