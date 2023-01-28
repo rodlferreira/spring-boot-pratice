6725a29dbe87811a0f02014db933103d6d50edda
rodlferreira <rodriggo.lferreira@icloud.com>
2023-01-28 15:30:36 -0300

    conexão com o banco de dados h2

--- src/main/java/org/springBoot/VendasApplication.java
+++ src/main/java/org/springBoot/VendasApplication.java
@@ -1,7 +1,5 @@
-package org.example;
+package org.springBoot;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,18 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 
-@RestController
 public class VendasApplication {
 
-    @Value("${application.name}")
-    private String applicationName;
-
-    @GetMapping("/hello")
-    public String helloworld(){
-        return applicationName;
-    }
-
     public static void main(String[] args) {
+
         SpringApplication.run(VendasApplication.class, args);
     }
 }
